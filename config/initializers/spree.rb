89f7799df9895aa333b04bf39b30b7f9bb57aca9
tbenedyk <tobybenedyk@gmail.com>
2014-10-12 19:13:58 +0100

    Add token secret to environment

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,23 +1,35 @@
-# Configure Spree Preferences
-#
-# Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
-#       If you would like users to be able to update a setting with the Admin it should NOT be set here.
-#
-# In order to initialize a setting do:
-# config.setting_name = 'new value'
 Spree.config do |config|
-  # Example:
-  # Uncomment to stop tracking inventory levels in the application
-  # config.track_inventory_levels = false
+  attachment_config = {
+    s3_credentials: {
+      access_key_id: "AKIAJGPCM7TFZUFGSHBA",
+      secret_access_key: "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",
+      bucket: "basement-images",
+    },
+ 
+    storage:        :s3,
+    s3_headers:     { "Cache-Control" => "max-age=31557600" },
+    s3_protocol:    "https",
+    bucket:         "basement-images",
+ 
+    styles: {
+      mini:     "48x48>",
+      small:    "100x100>",
+      product:  "240x240>",
+      large:    "600x600>"
+    },
+ 
+    path:          ":rails_root/public/spree/products/:id/:style/:basename.:extension",
+    default_url:   "/spree/products/:id/:style/:basename.:extension",
+    default_style: "product",
+  }
+ 
+  attachment_config.each do |key, value|
+    Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+  end
+end unless Rails.env.test?
 
-  config.use_s3 = true
-  config.s3_bucket = "basement-images"
-  config.s3_access_key = "AKIAJGPCM7TFZUFGSHBA"
-  config.s3_secret = "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"
-end
-
-Spree.user_class = "Spree::User"
-
-Paperclip.interpolates(:s3_eu_url) do |attachment, style|
-  "#{attachment.s3_protocol}://#{Spree::Config[:s3_host_alias]}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
-end
+# Spree.user_class = "Spree::User"
+# 
+# Paperclip.interpolates(:s3_eu_url) do |attachment, style|
+  # "#{attachment.s3_protocol}://#{Spree::Config[:s3_host_alias]}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
+# end
