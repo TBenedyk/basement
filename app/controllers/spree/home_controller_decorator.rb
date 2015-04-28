2968934c6fcf1e1eabfdb6060f0d6ddec7d9a7fc
tbenedyk <tobybenedyk@gmail.com>
2015-04-28 17:27:04 +0100

    limited homepage products to 20

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -9,12 +9,10 @@ Spree::HomeController.class_eval do
     @slider_products = Spree::Product.first(8)
 
     featured = Spree::Taxon.where(:name => 'Featured').first
-    @featured_products = featured.products.active.first(20).reverse if featured
-    @featured_products = @featured_products.select(uniq_method) if featured
+    @featured_products = featured.products.active.last(20).reverse if featured
 
     latest = Spree::Taxon.where(:name => 'Latest').first
     @latest_products = latest.products.active.last(20).reverse if latest
-    @latest_products = @latest_products.select(uniq_method) if latest
 
   end
 
