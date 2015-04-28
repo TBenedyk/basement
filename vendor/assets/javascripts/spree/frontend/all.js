f65bbbd3b21bf446759e4044a88ea7528bbfbb2b
tbenedyk <tobybenedyk@gmail.com>
2015-04-28 18:16:39 +0100

    changed slideshow image settings

--- vendor/assets/javascripts/spree/frontend/all.js
+++ vendor/assets/javascripts/spree/frontend/all.js
@@ -21,44 +21,6 @@ $(function(){
   // Hide flash messages after timeout
   setTimeout('$(".flash").fadeOut(1000)', 5000);
 
-  // Home sliders
-  if($('#home-slider').length > 0) {
-
-    var cached_carousel_1 = $('#featured-products .carousel').html();
-    var cached_carousel_2 = $('#latest-products .carousel').html();
-
-    $('#home-slider > ul').bxSlider({
-      auto: true,
-      autoHover: true,
-      useCSS: true,
-      controls: false,
-      pagerSelector: '.slider-pager',
-      touchEnabled: true
-    });
-
-    $('.carousel').bxSlider({
-      minSlides: 1,
-      maxSlides: 4,
-      useCSS: true,
-      slideWidth: 230,
-      slideMargin: 10,
-      auto: true,
-      autoHover: true,
-      controls: false,
-      touchEnabled: true
-    });
-
-    $("#home-slider .product-description").dotdotdot({
-      watch: true,
-      height: 250
-    });
-
-    $("#home-slider").closest(".container").css("width", "auto");
-    $("#home-slider").closest(".subheader-wrapper").css("padding", "0");
-    $("#home-slider li").css("width", $(window).width());
-
-  }
-
   // Make buttons from radio inoputs
   $( "#product-variants .variants-buttons" ).buttonset();
   $( ".payment-method-selector").buttonset();
@@ -214,4 +176,43 @@ $(document).ready(function() {
   }), function() {
     $(this).find("ul").hide();
   });
+});
+
+$(window).load(function() {
+  if($('#home-slider').length > 0) {
+
+    var cached_carousel_1 = $('#featured-products .carousel').html();
+    var cached_carousel_2 = $('#latest-products .carousel').html();
+
+    $('#home-slider > ul').bxSlider({
+      auto: true,
+      autoHover: true,
+      useCSS: true,
+      controls: false,
+      pagerSelector: '.slider-pager',
+      touchEnabled: true
+    });
+
+    $('.carousel').bxSlider({
+      minSlides: 1,
+      maxSlides: 4,
+      useCSS: true,
+      slideWidth: 230,
+      slideMargin: 10,
+      auto: true,
+      autoHover: true,
+      controls: false,
+      touchEnabled: true
+    });
+
+    $("#home-slider .product-description").dotdotdot({
+      watch: true,
+      height: 250
+    });
+
+    $("#home-slider").closest(".container").css("width", "auto");
+    $("#home-slider").closest(".subheader-wrapper").css("padding", "0");
+    $("#home-slider li").css("width", $(window).width());
+
+  }
 });
