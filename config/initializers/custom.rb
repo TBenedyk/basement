5e7152c5c9bab95af6ce2a962cf7aec170564c20
tbenedyk <tobybenedyk@gmail.com>
2015-05-04 14:28:48 +0100

    only look up sale prices

--- config/initializers/custom.rb
+++ config/initializers/custom.rb
@@ -13,31 +13,31 @@ Spree::Core::Search::Base.class_eval do
 	    		if encoded_scope.include?("Under+%C2%A310.00+GBP")
 	    			sale_price_ids = Spree::SalePrice.where("value < ?", 10).map(&:price_id)
 					product_ids << Spree::Price.where("id IN (?)", sale_price_ids).map { |p| p.variant.product_id}
-	    			product_ids << Spree::Price.where("amount < ? AND spree_prices.id NOT IN (?)", 10, sale_price_ids).map { |p| p.variant.product_id}	    		
+	    			#product_ids << Spree::Price.where("amount < ? AND spree_prices.id NOT IN (?)", 10, sale_price_ids).map { |p| p.variant.product_id}	    		
 	    		end
 
 	    		if encoded_scope.include?("%C2%A310.00+GBP+-+%C2%A315.00+GBP")
 	    			sale_price_ids = Spree::SalePrice.where("value > ? AND value < ?", 10, 15).map(&:price_id)
 	    			product_ids << Spree::Price.where("id IN (?)", sale_price_ids).map { |p| p.variant.product_id}
-	    			product_ids << Spree::Price.where("amount > ? AND amount < ? AND spree_prices.id NOT IN (?)", 10, 15, sale_price_ids).map { |p| p.variant.product_id}
+	    			#product_ids << Spree::Price.where("amount > ? AND amount < ? AND spree_prices.id NOT IN (?)", 10, 15, sale_price_ids).map { |p| p.variant.product_id}
 	    		end
 
 	    		if encoded_scope.include?("%C2%A315.00+GBP+-+%C2%A318.00+GBP")
 	    			sale_price_ids = Spree::SalePrice.where("value > ? AND value < ?", 15, 18).map(&:price_id)
 					product_ids << Spree::Price.where("id IN (?)", sale_price_ids).map { |p| p.variant.product_id}
-	    			product_ids << Spree::Price.where("amount > ? AND amount < ? AND spree_prices.id NOT IN (?)", 15, 18, sale_price_ids).map { |p| p.variant.product_id}
+	    			#product_ids << Spree::Price.where("amount > ? AND amount < ? AND spree_prices.id NOT IN (?)", 15, 18, sale_price_ids).map { |p| p.variant.product_id}
 	    		end
 
 	    		if encoded_scope.include?("%C2%A318.00+GBP+-+%C2%A320.00+GBP")
 	    			sale_price_ids = Spree::SalePrice.where("value > ? AND value < ?", 18, 20).map(&:price_id)
 					product_ids << Spree::Price.where("id IN (?)", sale_price_ids).map { |p| p.variant.product_id}
-	    			product_ids << Spree::Price.where("amount > ? AND amount < ? AND spree_prices.id NOT IN (?)", 18, 20, sale_price_ids).map { |p| p.variant.product_id}
+	    			#product_ids << Spree::Price.where("amount > ? AND amount < ? AND spree_prices.id NOT IN (?)", 18, 20, sale_price_ids).map { |p| p.variant.product_id}
 	    		end
 
 	    		if encoded_scope.include?("%C2%A320.00+GBP+or+over")
 	    			sale_price_ids = Spree::SalePrice.where("value > ?", 20).map(&:price_id)
 					product_ids << Spree::Price.where("id IN (?)", sale_price_ids).map { |p| p.variant.product_id}
-	    			product_ids << Spree::Price.where("amount > ? AND spree_prices.id NOT IN (?)", 20, sale_price_ids).map { |p| p.variant.product_id}	    		
+	    			#product_ids << Spree::Price.where("amount > ? AND spree_prices.id NOT IN (?)", 20, sale_price_ids).map { |p| p.variant.product_id}	    		
 	    		end
 
 	    		base_scope = base_scope.merge(Spree::Product.where("spree_products.id IN (?)", product_ids.flatten.uniq))
